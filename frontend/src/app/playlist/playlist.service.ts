3c7c3d80ac0c653f7ab01904b582a0b876ebc4f2
bernmic <Bermel22>
2018-11-22 15:24:33 +0100

    Bugs

--- frontend/src/app/playlist/playlist.service.ts
+++ frontend/src/app/playlist/playlist.service.ts
@@ -25,7 +25,7 @@ export class PlaylistService {
   }
 
   addSongsToPlaylist(playlistId: string, songIds: string[]): Observable<any> {
-    return this.http.post(environment.restserver + "/api/playlist/" + playlistId + "/artists", songIds);
+    return this.http.post(environment.restserver + "/api/playlist/" + playlistId + "/songs", songIds);
   }
 
   savePlaylist(playlist: Playlist): Observable<Playlist> {
